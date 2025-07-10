0625a9790185b238a9a3b445f5fd8e507c30c30b
zkstasi <zkstasy@gmail.com>
2025-07-10 16:55:34 +0300

    update repo/logger

--- internal/repository/repo.go
+++ internal/repository/repo.go
@@ -52,10 +52,10 @@ func Logger(stop <-chan struct{}) {
 
 	// предыдущие длины слайс
 
-	lastOrdersCount := 0
-	lastUsersCount := 0
-	lastDeliveriesCount := 0
-	lastWarehousesCount := 0
+	lastOrdersIndex := 0
+	lastUsersIndex := 0
+	lastDeliveriesIndex := 0
+	lastWarehousesIndex := 0
 
 	for {
 		select {
@@ -64,35 +64,53 @@ func Logger(stop <-chan struct{}) {
 		default:
 			muOrders.Lock()
 			ordersCount := len(orders)
+			newOrders := orders[lastOrdersIndex:ordersCount]
 			muOrders.Unlock()
 
 			muUsers.Lock()
 			usersCount := len(users)
+			newUsers := users[lastUsersIndex:usersCount]
 			muUsers.Unlock()
 
 			muDeliveries.Lock()
 			deliveriesCount := len(deliveries)
+			newDeliveries := deliveries[lastDeliveriesIndex:deliveriesCount]
 			muDeliveries.Unlock()
 
 			muWarehouses.Lock()
 			warehousesCount := len(warehouses)
+			newWarehouses := warehouses[lastWarehousesIndex:warehousesCount]
 			muWarehouses.Unlock()
 
-			if ordersCount > lastOrdersCount {
-				fmt.Printf("New orders: %d\n", ordersCount-lastOrdersCount)
-				lastOrdersCount = ordersCount
+			if len(newOrders) > 0 {
+				fmt.Printf("New orders: %d\n", len(newOrders))
+				for _, o := range newOrders {
+					fmt.Printf("Order ID: %s, UserID: %s, Status: %d, CreatedAt: %s\n", o.Id(), o.UserId(), o.Status(), o.CreatedAt())
+				}
+				lastOrdersIndex = ordersCount
 			}
-			if usersCount > lastUsersCount {
-				fmt.Printf("New users: %d\n", usersCount-lastUsersCount)
-				lastUsersCount = usersCount
+			if len(newUsers) > 0 {
+				fmt.Printf("New users: %d\n", len(newUsers))
+				for _, u := range newUsers {
+					fmt.Printf("User ID: %s, Name: %s\n", u.Id(), u.Name())
+				}
+				lastUsersIndex = usersCount
 			}
-			if deliveriesCount > lastDeliveriesCount {
-				fmt.Printf("New deliveries: %d\n", deliveriesCount-lastDeliveriesCount)
-				lastDeliveriesCount = deliveriesCount
+
+			if len(newDeliveries) > 0 {
+				fmt.Printf("New deliveries: %d\n", len(newDeliveries))
+				for _, d := range newDeliveries {
+					fmt.Printf("Delivery ID: %d, OrderID: %s, UserID: %s, Address: %s, Status: %d\n", d.Id(), d.OrderId(), d.UserId(), d.Address(), d.Status())
+				}
+				lastDeliveriesIndex = deliveriesCount
 			}
-			if warehousesCount > lastWarehousesCount {
-				fmt.Printf("New warehouses: %d\n", warehousesCount-lastWarehousesCount)
-				lastWarehousesCount = warehousesCount
+
+			if len(newWarehouses) > 0 {
+				fmt.Printf("New warehouses: %d\n", len(newWarehouses))
+				for _, w := range newWarehouses {
+					fmt.Printf("Warehouse ID: %d, OrderID: %s, Status: %d\n", w.Id(), w.OrderId(), w.Status())
+				}
+				lastWarehousesIndex = warehousesCount
 			}
 		}
 
